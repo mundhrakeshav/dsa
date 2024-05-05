08fa35a6fb02ca3592fac9470d3d46825430b607
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-05-05 18:11:53 +0530

    feat: Add MajorityElement2 function and test

--- golang-dsa/Problems/majority_element/majority_element.go
+++ golang-dsa/Problems/majority_element/majority_element.go
@@ -1,7 +1,7 @@
 package majorityelement
 
-func MajorityElement(arr []int) (int){
-	count, element := 0, 0 
+func MajorityElement2(arr []int) int {
+	count, element := 0, 0
 	for _, val := range arr {
 		if count == 0 {
 			element = val
@@ -18,8 +18,59 @@ func MajorityElement(arr []int) (int){
 			count++
 		}
 	}
-	if count > len(arr) / 2 {
+	if count > len(arr)/2 {
 		return element
 	}
 	return -1
+}
+
+func MajorityElement3(arr []int) [2]int {
+	count1, count2, element1, element2 := 0, 0, 0, 0
+	for _, v := range arr {
+		switch {
+		case count1 == 0 && v != element2:
+			{
+				count1++
+				element1 = v
+			}
+		case count2 == 0 && v != element1:
+			{
+				count2++
+				element2 = v
+			}
+		case v == element1:
+			{
+				count1++
+			}
+		case v == element2:
+			{
+				count2++
+			}
+		default:
+			{
+				count1--
+				count2--
+			}
+		}
+	}
+	count1, count2 = 0, 0
+
+	for _, v := range arr {
+		if v == element1 {
+			count1++
+		} else if v == element2 {
+			count2++
+		}
+	}
+
+	ret := [2]int{-1, -1}
+	if count1 > len(arr) / 3 {
+		ret[0] = element1
+	}
+	
+	if count2 > len(arr) / 3 {
+		ret[1] = element2
+	}
+
+	return ret
 }
--- golang-dsa/Problems/majority_element/majority_element_test.go
+++ golang-dsa/Problems/majority_element/majority_element_test.go
@@ -7,7 +7,11 @@ import (
 	majorityelement "github.com/mundhrakeshav/DSA/golang-dsa/Problems/majority_element"
 )
 
-func TestMajorityElement(t *testing.T) {
+func TestMajorityElement2(t *testing.T) {
 	arr := []int{2, 2, 1, 1, 1, 1, 1, 2, 2}
-	fmt.Println(majorityelement.MajorityElement(arr))
+	fmt.Println(majorityelement.MajorityElement2(arr))
+}
+func TestMajorityElement3(t *testing.T) {
+	arr := []int{2, 2, 1, 1, 1, 1, 1, 2, 3, 3, 2}
+	fmt.Println(majorityelement.MajorityElement3(arr))
 }
