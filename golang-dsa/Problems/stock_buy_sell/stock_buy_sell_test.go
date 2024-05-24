2f170dc73123c2ed076cf215d6d80c60b2d0a0e3
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-05-24 15:30:53 +0530

    Refactor stock buy sell test cases

--- golang-dsa/Problems/stock_buy_sell/stock_buy_sell_test.go
+++ golang-dsa/Problems/stock_buy_sell/stock_buy_sell_test.go
@@ -7,8 +7,22 @@ import (
 	stockbuysell "github.com/mundhrakeshav/DSA/golang-dsa/Problems/stock_buy_sell"
 )
 
-func TestStockBuySell(t *testing.T) {
-	fmt.Println(stockbuysell.StockBuySell([]int64{
-		7, 1, 5, 3, 6, 4,
-	}))
+func TestStockBuySellMultipleCases(t *testing.T) {
+	tests := []struct {
+		input    []int64
+		expected int64
+	}{
+		{[]int64{7, 1, 5, 3, 6, 4}, 5},
+		{[]int64{7, 6, 4, 3, 1}, 0},
+		{[]int64{2, 4, 1}, 2},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("Input: %v", test.input), func(t *testing.T) {
+			actual, _ := stockbuysell.StockBuySell(test.input)
+			if actual != test.expected {
+				t.Errorf("Expected: %d, Got: %d", test.expected, actual)
+			}
+		})
+	}
 }
