a315f20749c8b8cd256114a5ea4c6e55e3445f20
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-06-10 14:25:20 +0530

    TODO: LeetCode:974

---
+++ golang-dsa/LeetCode/golang-leetcode/medium/974.subarray-sums-divisible-by-k.go
@@ -0,0 +1,22 @@
+package leetcode_medium
+
+/*
+ * @lc app=leetcode id=974 lang=golang
+ *
+ * [974] Subarray Sums Divisible by K
+ */
+
+// @lc code=start
+func SubarraysDivByK(nums []int, k int) int {
+	return subarraysDivByK(nums, k)
+}
+
+// TODO
+func subarraysDivByK(nums []int, k int) int {
+	// map[val]index
+	count := 0
+
+	return count
+}
+
+// @lc code=end
