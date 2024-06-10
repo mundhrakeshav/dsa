262b7cc1140b7b78c7f82b4d3fea97a93395186b
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-06-10 22:21:09 +0530

    ugh, i don't like this problem

--- golang-dsa/LeetCode/golang-leetcode/easy/1051.height-checker.go
+++ golang-dsa/LeetCode/golang-leetcode/easy/1051.height-checker.go
@@ -1,5 +1,7 @@
 package easy
 
+import "sort"
+
 /*
  * @lc app=leetcode id=1051 lang=golang
  *
@@ -8,7 +10,17 @@ package easy
 
 // @lc code=start
 func heightChecker(heights []int) int {
-    
+    temp := make([]int, 0)
+	temp = append(temp, heights...)
+
+	sort.Ints(temp)
+	count := 0
+	for i, v := range heights {
+		if temp[i] != v {
+			count++
+		}
+	}
+	return count
 }
 // @lc code=end
 
